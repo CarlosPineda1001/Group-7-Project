7404de6ac688556122ea41646d3cfcdb380b78bb
Carlos Miguel M. Pineda <carlosmiguel.pineda.iics@ust.edu.ph>
2021-11-29 18:59:23 +0800

    system can now recognize the account who created a document group

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const crypto = require('crypto');
-
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
 
 const ViewPage_DefaultRoutes = require('./routes/ViewPage_DefaultRoutes');
-const Document_DefaultRoutes = require('./routes/Document_DetailsRoutes');
+//const Document_DefaultRoutes = require('./routes/Document_DetailsRoutes');
 
 const Acc = require('./Models/account_Schema');
+const Doc = require('./Models/document_Schema');
 
 //express app
 //instance of express app
@@ -59,9 +62,6 @@ app.use(express.urlencoded({extended: true})); //used for accepting form data
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
-
-
-
 // connect to mongodb
 const dbURI = 'mongodb+srv://Carlos:XpaZ@mongouploads.zxnhp.mongodb.net/Document_Database?retryWrites=true&w=majority';
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -72,10 +72,36 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     })
     .catch((err) => console.log(err));
 
+let datab = mongoose.connection;
+
+datab.once('open', () => {
+    gfs = Grid(datab.db, mongoose.mongo);
+    gfs.collection('docs');
+})
+
+// create storage engine
+const storage = new GridFsStorage({
+    url: dbURI,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex');
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'docs'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+
+const upload = multer({ storage })
+
 //lsten to what page
-/*app.get('/', (req, res) =>{
-    res.render('Loginpage', {title: name});
-});*/
 
 app.get('/register', (req, res) =>{
     if(logged_in){
@@ -144,18 +170,52 @@ app.post('/', (req,res)=>{
                   //  console.log("email exists!");
                     console.log("nakalogin kana boy");
                     console.log(user);
+                    userNow = user.l_Name + ", " + user.f_Name;
                    res.redirect('/');
                    // if(pass == )
                    logged_in = true;
                 }else{
                     
-            
-                      }
+                }
             })
 
     console.log(email);
     console.log(pass);
 
+});
+
+app.post('/ViewPage_Default',upload.array('file',12), (req,res, next)=>{
+    //file_ID array
+    let fileIDs = [];
+    req.files.forEach((file, index)=>{
+        
+        fileIDs[index]=file.filename
+         return fileIDs;
+     })
+    const doc = new Doc({
+        
+        docu_Group: req.body.docu_Group,
+        docu_Type: req.body.docu_Type,
+        file_ID: fileIDs,
+        created_By: userNow
+       
+    });
+    doc.save()
+    .then(result => {
+      res.redirect('/ViewPage_Default');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/Document_Details/:id', (req, res) =>{
+    const id = req.params.id;
+
+    Doc.findById(id)
+        .then(result => {
+            res.render('PreviewDetails', {doc: result});
+        });
 });
 
 
@@ -230,7 +290,7 @@ app.post('/register', (req,res)=>{
 app.use('/ViewPage_Default', ViewPage_DefaultRoutes);
 
 // route for DocumentDetailPage
-app.use('/Document_Details', Document_DefaultRoutes);
+//app.use('/Document_Details', Document_DefaultRoutes);
 
 //404 page
 app.use((req,res)=>{
--- routes/Document_DetailsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const DocumentDetailsController = require('../controllers/DocumentDetailsController')
-
-const router = express.Router();
-
-router.get('/:id', DocumentDetailsController.viewDocDetails);
-
-module.exports = router;
