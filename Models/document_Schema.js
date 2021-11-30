8b0faf0f805e6672c50e6034832ea74d7ae7129c
Carlos Miguel M. Pineda <carlosmiguel.pineda.iics@ust.edu.ph>
2021-11-30 15:23:19 +0800

    standardized the date_Created

--- Models/document_Schema.js
+++ Models/document_Schema.js
@@ -2,6 +2,42 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //template of the document files
 
+const date = new Date(Date.now());
+
+const dateMonth = date.toString().slice(4,7);
+
+switch(dateMonth){
+    case 'Jan' : dateMonthNew = '01';
+    break;
+    case 'Feb' : dateMonthNew = '02';
+    break;
+    case 'Mar' : dateMonthNew = '03';
+    break;
+    case 'Apr' : dateMonthNew = '04';
+    break;
+    case 'May' : dateMonthNew = '05';
+    break;
+    case 'Jun' : dateMonthNew = '06';
+    break;
+    case 'Jul' : dateMonthNew = '07';
+    break;
+    case 'Aug' : dateMonthNew = '08';
+    break;
+    case 'Sep' : dateMonthNew = '09';
+    break;
+    case 'Oct' : dateMonthNew = '10';
+    break;
+    case 'Nov' : dateMonthNew = '11';
+    break;
+    case 'Dec' : dateMonthNew = '12';
+    break;
+}
+
+const dateDay = date.toString().slice(8,10);
+
+const dateYear = date.toString().slice(11,15);
+
+const dateNow = dateMonthNew + "/" + dateDay + "/" + dateYear;
 
 const documentSchema =  new Schema({
     file_ID:[String],
@@ -26,7 +62,7 @@ const documentSchema =  new Schema({
        // required: true
     },
     date_Created:{
-        type: Date, default: Date.now()
+        type: String, default: dateNow
        // required: true
     },
     date_Lmodified:{
